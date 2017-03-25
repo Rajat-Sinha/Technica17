ca5e937e2f29a75efffae5c19dc427d5fddc3738
Rajat-Sinha <2015UGMM039@nitjsr.ac.in>
2017-03-26 04:29:15 +0530

    Add files via upload

---
+++ live/feedback.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700|Droid+Serif' rel='stylesheet' type='text/css'>
+	<link href="css/bootstrap.css" type="text/css" rel="stylesheet">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+  	
+	<title>Live Broadcast</title>
+	<style>
+	
+	</style>
+</head>
+<body  style="background-color:#5c8a8a;margin-top:20px" >
+	<div class="container" style="background-color:darkslategray;">
+		<div class = "row">
+			<!--<h3 style="text-align:center;color:#00f;font:bold 55px Sans-serif " ><text>Technica'16 </text><text style="font-size:30px"> Video FeedForm</text></h3>-->
+		</div>
+		<!--<div class = "row" style="margin-top:10px">
+			<div class="col-lg-1 col-md-1 col-sm-1">
+				<a href="http://technica.net.in"style="text-align:Left;color:#000;font:bold 25px Sans-serif " >Home</a>
+			</div>
+			<div class="col-lg-10 col-md-10 col-sm-10"></div>
+			<div class="col-lg-1 col-md-1 col-sm-1">
+				<a href="http://smes.co.in"style="text-align:Left;color:#000;font:bold 25px Sans-serif " >SMES</a>
+			</div>
+		</div>--><br><br><br>
+		<hr style="color:black;wigth:500px">
+		<div class = "row" style="margin-top:20px">
+			<div class="col-lg-7 col-md-12 col-sm-12">
+				<div class="row embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" width="600" height="170" src="http://www.ustream.tv/embed/22930203?html5ui" scrolling="no" allowfullscreen webkitallowfullscreen frameborder="0" style="border: 0 none transparent;"></iframe>
+					<br />
+					<div class="well" style="background-color:#09c;height:10px;width:600px">
+						<h4 style="color:white;margin-top:-10px;text-align:center;">Feed from Cam 1</h4>
+					</div>
+				</div>
+				<br /><br />
+				<div class="row embed-responsive embed-responsive-16by9" style="margin-top:-10px">
+					<iframe class="embed-responsive-item" width="600" height="270" src="http://www.ustream.tv/embed/22932132?html5ui" scrolling="no" allowfullscreen webkitallowfullscreen frameborder="0" style="border: 0 none transparent;"></iframe>
+					<div class="well" style="background-color:#09c;height:10px;width:600px">
+						<h4 style="color:white;margin-top:-10px;text-align:center;">Feed from Cam 2</h4>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-5 col-md-12 col-sm-12 well" style="background-color:#333;height:780px">
+				<h3 style="color:white;font:bold 30px helvetica;text-align:center">Time Schedule</h3>
+				<hr/>
+				<img class="img-responsive" src="sche.jpg" width="420" height="auto" style="margin-top:-12px;width:420px;height:640px;"/>
+			</div> 
+		</div>
+	</div>
+</body>
+</html>
+		
